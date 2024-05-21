a1006dfc760bde253c5c4705f33b705422788871
Luis Vanegas <lvanegas1429@gmail.com>
2024-05-21 10:14:25 -0600

    creacion de store lista

--- components/modal/store-modal.tsx
+++ components/modal/store-modal.tsx
@@ -32,6 +32,7 @@ export const StoreModal = () => {
             setIsLoading(true)
             const response = await axios.post('/api/stores', values)
             toast.success('Store Created')
+            window.location.assign(`/${response.data.id}`)
         } catch (error) {
             toast.error('Something went wrong')
         } finally {
